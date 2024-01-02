da62c4867702961141e726b7e31bc3c29489d034
unimat <unimat@unimat.hu>
2024-01-02 14:12:12 +0100

    Add Home Header Component

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import HomeHeader from "@/components/HomeHeader";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home Page</h1>
-    </main>    
+    <>
+      <HomeHeader />
+    </>
   )
 }
