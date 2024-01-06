091bd819a170afc1486706835025caf03b3fa04f
unimat <unimat@unimat.hu>
2024-01-06 12:58:39 +0100

    Add Body Section

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '**/640px-Google_2015_logo.svg.png'
+      }
+    ]
+  },
+}
 
 module.exports = nextConfig
